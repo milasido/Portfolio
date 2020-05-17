5a1f70411701b2c9d3120fa8631c36cec110eb5c
nguyen wol <wolnguyen98@gmail.com>
2020-05-17 15:23:20 -0500

    change path

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -22,7 +22,7 @@ export default class navbar extends Component{
                     <ul className="flex-md-column flex-row navbar-nav w-100 justify-content-between">
                     
                     <li className="nav-logo">
-                        <Link to="/" style={{ textDecoration: 'none' }}><img className="logo" src={logo}/></Link>
+                        <Link to="/Portfolio" style={{ textDecoration: 'none' }}><img className="logo" src={logo}/></Link>
                     </li>
                     <li className="nav-item">
                         <Link to="/projects" style={{ textDecoration: 'none' }}><a className="nav-link pl-0 "><span className="d-md-inline">Projects</span></a></Link>
@@ -41,7 +41,7 @@ export default class navbar extends Component{
                 </nav> 
             </aside>          
             <Switch>
-                <Route exact path="/" component={HOME}/>
+                <Route exact path="/Portfolio" component={HOME}/>
                 <Route exact path="/resume" component={RESUME}/>
                 <Route exact path="/projects" component={PROJECTS}/>
                 <Route exact path="/contact" component={CONTACT}/>
